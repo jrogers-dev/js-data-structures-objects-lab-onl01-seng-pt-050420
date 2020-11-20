de9949149e4ef54a851dc69e0ed0df225c48053d
James Rogers <jrogers.dev@gmail.com>
2020-11-20 22:43:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return newDriver;
 }
 
-function destructivelyUpdateDriverWithKeyAndValue() {
-  
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
 }
 
-function deleteFromDriverByKey() {
-  
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = { ...driver };
+  delete newDriver[key];
+  return newDriver;
 }
 
-function destructivelyDeleteFromDriverByKey() {
-  
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
 }
 
